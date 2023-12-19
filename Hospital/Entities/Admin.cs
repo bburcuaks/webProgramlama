7b763da2440d6b85992fcd9c74eeeeb2d1e5681d
bburcuaks <104011386+bburcuaks@users.noreply.github.com>
2023-12-19 22:03:28 +0300

    ..

---
+++ Hospital/Entities/Admin.cs
@@ -0,0 +1,14 @@
+﻿namespace Hospital.Entities
+{
+    public class Admin:User
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public List<Doctor> Doctors{ get; set; }
+        public List<Patient> Patients { get; set; }
+        public List<Appointment> Appointments{ get; set; }
+
+    }
+}
--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -19,7 +19,7 @@ builder.Services
         opts.ExpireTimeSpan = TimeSpan.FromDays(7);
         opts.SlidingExpiration = false;
         opts.LoginPath = "/Account/Login";
-        opts.LoginPath = "/Account/Logout";
+        opts.LogoutPath = "/Account/Logout";
         opts.AccessDeniedPath = "/Home/AccessDenied";
     });
 var app = builder.Build();
